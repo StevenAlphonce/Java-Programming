122858aac2ad35642de51a444e1da2e41e3d9f82
Alphonce Stephano <alphoncesteven@gmail.com>
2021-05-25 23:30:23 +0300

    Add files via upload
    Assignment: Write a program that prompts you to enter an ID (integer type), full name (first-
    name surname), and several exam scores (integer type) for a student. Your program should
    
    process the name to be in the format Surname, first-name initial (e.g. Messi, L. from Lionel
    Messi). The program should also print the average of the entered scores. Sample input and
    output should look as follows:

---
+++ lab4Qn2.java
@@ -0,0 +1,29 @@
+package lab4;
+import java.util.Scanner;
+public class lab4Qn2 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int sum=0,countCourse=0,items=1;
+		boolean validator=true;
+		System.out.printf("Enter student's ID:");
+		int studentId=scan.nextInt();
+		scan.nextLine();
+		System.out.printf("Enter the student's Full name:");
+		char firstName=scan.next().charAt(0);
+		String lastName=scan.next();
+			while (validator)
+			{
+			System.out.printf("Enter student's course score %d or a non-integer number when done :",items++);
+			if(!scan.hasNextInt())
+			{
+			validator=false;
+			break;
+			}
+			int num=scan.nextInt();
+			sum+=num;
+			countCourse++;
+			}
+			float average=sum/countCourse;
+			System.out.printf("My name is %s,%C.with the ID %d. My average score is: %.2f",lastName,firstName,studentId,average);
+		}
+}
